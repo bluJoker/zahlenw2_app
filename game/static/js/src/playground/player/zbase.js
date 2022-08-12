5b9da1757e471c6ce1006d6466d8638913c5ab77
wei wang <blumeng@163.com>
2022-08-12 11:46:57 +0800

    implement player moving

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -5,6 +5,11 @@ class Player extends AcGameObject {
 	this.playground = playground;
 	this.x = x;
 	this.y = y;
+	//vx, vy:速度
+	this.vx = 0;
+	this.vy = 0;
+	//move_length:移动距离
+	this.move_length = 0;
         this.ctx = this.playground.game_map.ctx;
         this.radius = radius;
 	this.color = color;
@@ -13,14 +18,57 @@ class Player extends AcGameObject {
 	// 精度:浮点运算中小于多少算0
 	this.eps = 0.1;
     }
-    start() {}
+    
+    add_listening_events() {
+	let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu", function() {
+	    return false;
+	});
+	this.playground.game_map.$canvas.mousedown(function(e) {
+	    //3:鼠标右键, 1:左键, 2:滚轮
+	    if (e.which === 3) {
+		outer.move_to(e.clientX, e.clientY);
+	    }
+	});
+    }
+
+    get_dist(x1, y1, x2, y2) {
+        let dx = x1 - x2;
+	let dy = y1 - y2;
+	return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    move_to(tx, ty) {
+	//console.log("move to", tx, ty);
+	this.move_length = this.get_dist(this.x, this.y, tx, ty);
+	//求角度 反正切
+	let angle = Math.atan2(ty - this.y, tx - this.x);
+	//vx:横方向速度, vy:纵方向速度
+	this.vx = Math.cos(angle);
+	this.vy = Math.sin(angle);
+    }
+
+    start() {
+	if (this.is_me) {
+	    this.add_listening_events();
+	}
+    }
     update() {
+	if (this.move_length < this.eps) {
+	    this.move_length = 0;
+	    this.vx = this.vy = 0;
+	} else {
+ 	    let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
+	    this.x += this.vx * moved;
+	    this.y += this.vy * moved;
+	    this.move_length -= moved;
+	}
 	this.render();
     }
     render() {
 	this.ctx.beginPath();
 	this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-	console.log(this.x, this.y, this.radius);
+	//console.log(this.x, this.y, this.radius);
 	this.ctx.fillStyle = this.color;
 	this.ctx.fill();
     }
